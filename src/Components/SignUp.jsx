93a1abe0b0ccfd4142dcc6ce4a517f51e8f0071a
ynparmar07 <ymparmar7@gmail.com~>
2024-07-01 18:42:26 +0530

    fix err

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,14 +1,102 @@
-import React from "react";
-import Left from "./Left";
-import Right  from "./Right";
+import React, { useState } from "react";
+
 import './Style/signup.css'
 
 const SignUp = () => {
+
+
+  const [details, setDetails] = useState({ email: "", fullname: "", password: "" });
+const [isSubmit, setIsSubmit] = useState(false);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log("Your details:", details);
+  setIsSubmit(true);
+};
+
+const handleChange = (event) => {
+  const { name, value } = event.target;
+  setDetails((prevDetails) => ({
+    ...prevDetails,
+    [name]: value
+  }));
+};
+
   return (
     <div id="main">
-      <Left />
-      <Right />
+      <div id="left">
+        <div id="logo">
+          <img src="/logo.png" alt="better market logo" />
+        </div>
+        <div id="content">
+
+          Say hello to global food and beverage producers and suppliers, all in one place
+
+        </div>
       </div>
+
+      <div className="right">
+        <div className="title">
+          Let’s get started <br /> with a few simple steps
+        </div>
+        {isSubmit ? (
+          <div className="submitted">
+            Form Submitted Successfully! <br />
+            Email: {details.email} <br />
+            FullName: {details.fullname} <br />
+            Password: {details.password}
+          </div>
+        ) : (
+
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <div className="input">
+                <label htmlFor="Email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="Email"
+                  value={details.email}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="input">
+                <label htmlFor="Fullname">FullName</label>
+                <input
+                  type="text"
+                  name="fullname"
+                  id="Fullname"
+                  value={details.fullname}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="input">
+                <label htmlFor="Password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="Password"
+                  value={details.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit">Sign up</button>
+            </form>
+          </div>
+        )}
+        <div className="footer">
+          <div>
+            By signing up, you agree to our <a href="/">Terms of Service.</a>
+          </div>
+          <div>
+            Already have an account? <a href="/">Log in</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 };
 
